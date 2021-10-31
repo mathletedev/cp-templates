fb78b4d5c9d992c55d305b2e6e3ecc523f1459de
Delta Eames <mathletedev@gmail.com>
2021-10-30 22:15:27 -0700

    formatter

--- template.cpp
+++ template.cpp
@@ -1,15 +1,16 @@
+#include <math.h>
+
+#include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <map>
 #include <set>
-#include <algorithm>
-#include <math.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 #define FOR(i, n) for (int i = 0; i < n; ++i)
-#define EACH(e, a) for (const auto &e: a)
+#define EACH(e, a) for (const auto &e : a)
 #define DEB(v) write("[ DEBUG ] ", #v, " => ", v)
 #define mp make_pair
 #define pb push_back
@@ -26,46 +27,49 @@ typedef set<int> si;
 typedef set<string> ss;
 typedef pair<int, int> pii;
 
-template<typename T> void read(T &x) {
-	cin >> x;
-}
-template<typename F, typename ...R> void read(F &f, R &...r) {
-	read(f);
-	read(r...);
+template <typename T>
+void read(T &x) {
+  cin >> x;
 }
 
-template<typename T> void write(T x) {
-	cout << x;
-}
-template<typename F, typename ...R> void write(const F &f, const R &...r) {
-	write(f);
-	write(r...);
+template <typename F, typename... R>
+void read(F &f, R &...r) {
+  read(f);
+  read(r...);
 }
 
-void print() {
-	write("\n");
-}
-template<typename F, typename ...R> void print(const F &f, const R &...r) {
-	write(f);
-	if (sizeof...(r)) write(" ");
-
-	print(r...);
+template <typename T>
+void write(T x) {
+  cout << x;
 }
 
-void solve() {
-
+template <typename F, typename... R>
+void write(const F &f, const R &...r) {
+  write(f);
+  write(r...);
 }
+
+void print() { write("\n"); }
+
+template <typename F, typename... R>
+void print(const F &f, const R &...r) {
+  write(f);
+  if (sizeof...(r)) write(" ");
+
+  print(r...);
+}
+
+void solve() {}
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	int t;
-	read(t);
+  int t;
+  read(t);
 
-	while (t--) solve();
+  while (t--) solve();
 
-	return 0;
+  return 0;
 }
-
